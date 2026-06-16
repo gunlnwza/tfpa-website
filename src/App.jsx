53a430d4be0232b4e71e2d4ef21c6bc6cd90d2c5
Natachat Teecharcharn <nteechar@Natachats-MacBook-Air.local>
2026-06-16 15:52:50 +0700

    feat: initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <p className="text-3xl font-bold text-red-500">hello world</p>
+    </>
+  )
+}
+
+export default App
